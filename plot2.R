800d005adb3f85d038f23e5451b2156f25cdaec4
charlottekoh1998 <charlottekoh1998@hotmail.com>
2020-07-20 11:03:46 +0800

    Commit

---
+++ plot2.R
@@ -0,0 +1,35 @@
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(gridExtra)
+library(grid)
+
+#load data
+if(file.exists("household_power_consumption.txt")) {
+  data0 <- read.table("household_power_consumption.txt",
+                      sep = ";", header = TRUE, stringsAsFactors = FALSE)
+} else if(file.exists("exdata-data-household_power_consumption.zip")) {
+  data0 <- read.table(unz("exdata-data-household_power_consumption.zip",
+                          "household_power_consumption.txt"),
+                      sep = ";", header = TRUE, stringsAsFactors = FALSE)
+} else {
+  
+  temp <- tempfile()
+  download.file("https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip",temp)
+  data <- read.table(unz(temp, "household_power_consumption.txt"),
+                     sep = ";", header = TRUE, stringsAsFactors = FALSE)
+  unlink(temp)
+}
+
+#format data
+data0 <- tbl_df(data0)
+dataFil <- filter(data0, Date == "2/2/2007" | Date == "1/2/2007")
+dataFil <- mutate(dataFil, Date_Time = paste(Date, Time, sep = " "))
+dataFil$Date_Time <- strptime(dataFil$Date_Time, "%d/%m/%Y %H:%M:%S")
+dataFil[, 3:9] <- lapply(dataFil[, 3:9], as.numeric)
+dataFil <- dataFil[, c(10, 3:9)]
+dataFil <- rename(dataFil, Kitchen = Sub_metering_1,
+                  Laundry_room = Sub_metering_2, Water_heater_AC = Sub_metering_3)
+
+plot(dataFil$Date_Time, dataFil$Global_active_power, type = "l",
+     ylab = "Global Active Power (kilowatts)", xlab = "")
